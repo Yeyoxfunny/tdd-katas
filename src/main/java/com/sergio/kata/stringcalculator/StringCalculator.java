31fb6697c76af38d09ba05965322736b08ef0690
Sergio Morales <yeyox_114@hotmail.com>
2017-12-24 16:21:32 -0500

    Especificacion de caracteres especiales en StringCalculator TDD

--- src/main/java/com/sergio/kata/stringcalculator/StringCalculator.java
+++ src/main/java/com/sergio/kata/stringcalculator/StringCalculator.java
@@ -1,14 +1,47 @@
 package com.sergio.kata.stringcalculator;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
+	private static final String DEFAULT_DELIMITADOR = ",|\n";
+	private String numeros;
+	private String delimitadores;
+
+	private StringCalculator(String numeros) {
+		this(numeros, DEFAULT_DELIMITADOR);
+	}
+
+	private StringCalculator(String numeros, String delimitadores) {
+		this.numeros = numeros;
+		this.delimitadores = delimitadores;
+	}
+
 	public static int sumar(String input) {
+		Objects.requireNonNull(input);
 		if (input.isEmpty()) {
 			return 0;
 		}
-		return Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).sum();
+
+		StringCalculator calculator = soportaDiferentesDelimitadores(input) ? cambiarDelimitador(input)
+				: new StringCalculator(input);
+		return sumar(calculator);
+	}
+
+	private static boolean soportaDiferentesDelimitadores(String input) {
+		return input.startsWith("//");
+	}
+
+	private static StringCalculator cambiarDelimitador(String input) {
+		String[] line = input.split("\n", 2);
+		String delimitador = Pattern.quote(line[0].substring(2));
+		return new StringCalculator(line[1], delimitador);
+	}
+
+	private static int sumar(StringCalculator calculator) {
+		return Arrays.stream(calculator.numeros.split(calculator.delimitadores)).mapToInt(Integer::parseInt).sum();
 	}
 
 }
--- src/test/java/com/sergio/kata/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/sergio/kata/stringcalculator/StringCalculatorTest.java
@@ -1,7 +1,7 @@
 package com.sergio.kata.stringcalculator;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -20,6 +20,28 @@ public class StringCalculatorTest {
 	@Test
 	public void sumarMultiplesNumerosSeparadorPorComa() {
 		assertThat(StringCalculator.sumar("10,35,28"), is(73));
+	}
+
+	@Test
+	public void sumarNumerosDelimitadosPorNuevaLinea() {
+		assertThat(StringCalculator.sumar("24\n3"), is(27));
+	}
+
+	@Test
+	public void sumarNumerosDelimitadosPorComaONuevaLinea() {
+		assertThat(StringCalculator.sumar("24,32\n15"), is(71));
+	}
+
+	@Test
+	public void sumarNumerosEspecificandoDelimitador() {
+		assertThat(StringCalculator.sumar("//;\n52;13"), is(65));
+	}
+
+	@Test
+	public void sumarNumerosEspecificandoDelimitadorEspecial() {
+		assertThat(StringCalculator.sumar("//.\n67.12"), is(79));
+		assertThat(StringCalculator.sumar("//*\n13*8*2*5"), is(28));
+		assertThat(StringCalculator.sumar("//?\n43?89?45"), is(177));
 	}
 
 }
